44337dbebc5c0f2909b0e43dc24e361d2ba43411
Christian Villegas <cf18christian.villegas@iesjoandaustria.org>
2019-04-29 19:04:44 +0200

    First Commit

---
+++ src/punts/Punt.java
@@ -0,0 +1,31 @@
+package punts;
+
+public class Punt {
+	private int x;
+	private int y;
+	
+	public Punt() {
+		this(0,0);
+	}
+
+	public Punt(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+}
---
+++ src/punts/TestPunt.java
@@ -0,0 +1,45 @@
+package punts;
+
+import org.junit.*;
+
+public class TestPunt {
+    @Test
+        public void testPuntConstructor() {
+            // comprova que el constructor Punt() pugui ser cridat
+            Punt p = new Punt();
+        }
+    
+    @Test
+    public void testPuntBuit() {
+        // comprova que el constructor Punt() deixi a 0 els valors
+        Punt p = new Punt();
+        Assert.assertEquals(0, p.getX()); // Afirma que la coordenada x val 0
+        Assert.assertEquals(0, p.getY());
+    }
+    
+    @Test
+    public void testPuntSetXAssignaCorrectament() {
+        Punt p = new Punt();
+        p.setX(12);
+        Assert.assertEquals(12, p.getX());
+    }
+    
+    @Test
+    public void testPuntSetYAssignaCorrectament() {
+        Punt p = new Punt();
+        p.setY(12);
+        Assert.assertEquals(12, p.getY());
+    }
+    
+    @Test
+    public void testPuntDefinit() {
+        // comprova que el constructor Punt(int,int) deixi els valors correctes
+        Punt p = new Punt(3, 4);
+        Assert.assertEquals(3, p.getX());
+        Assert.assertEquals(4, p.getY());
+    }
+    
+    public static void main(String args[]) {
+        org.junit.runner.JUnitCore.main("TestPunt");
+    }
+}
